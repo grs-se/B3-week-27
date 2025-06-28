8d71879017ed10ec5fa28215c1e2c3bd0e9409d7
ricesmg <george.rice-smith@pacsol.co.uk>
2025-06-28 23:43:59 +0100

    add ghpages docs build

--- challenge/B3-week-27-challenge--shopping-cart/src/environments/environment.production.ts
+++ challenge/B3-week-27-challenge--shopping-cart/src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:4200/assets/data/',
+  apiUrl: 'https://grs-se.github.io/B3-week-27/assets/data/',
   cartUrl: 'cart.json',
   productsUrl: 'products.json',
   ordersUrl: 'orders.json',
